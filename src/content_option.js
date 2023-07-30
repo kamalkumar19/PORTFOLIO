3a6b6aa336925e1f609b2941ff720f77b496e15c
kamalkumar19 <kamal.20b0131140@abes.ac.in>
2023-07-30 19:50:02 +0530

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,135 @@
+const logotext = "ARUN";
+const meta = {
+    title: "ARUN SHARMA",
+    description: "I’m ARUN SHARMA Web Developer.",
+};
+
+const introdata = {
+    title: "I’m ARUN SHARMA",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop Web apps",
+    },
+    description: "A Passionate Web Developer Building Digital Experiences with Precision and Creativity",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "",
+    aboutme: "Welcome to my web developer portfolio! I am a dedicated and skilled web developer who thrives on creating cutting-edge, user-centric websites and web applications. With a passion for technology and a keen eye for design, I am committed to delivering projects that not only meet clients' needs but also exceed their expectations.",
+};
+const worktimeline = [{
+        jobtitle: "Designer of week",
+        where: "YAdfi",
+        date: "2020",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Jamalya",
+        date: "2019",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "ALquds",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "C++",
+        value: 90,
+    },
+    {
+        name: "Html",
+        value: 85,
+    },
+    {
+        name: "Css",
+        value: 80,
+    },
+    {
+        name: "Javascript",
+        value: 70,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Welcome to our UI/UX services! We are a passionate team of designers and user experience experts dedicated to creating exceptional digital experiences that captivate and delight users. Our goal is to bridge the gap between your business objectives and the needs of your target audience, resulting in user interfaces that are not only visually appealing but also highly functional and user-friendly.",
+    },
+    {
+        title: "Web Apps",
+        description: "Welcome to our web-apps service! We are a team of skilled developers and designers committed to building custom web applications that cater to the unique needs of your business. Whether you're a startup, SME, or enterprise, our goal is to empower you with scalable and efficient web-based solutions that enhance productivity, streamline processes, and drive success.",
+    },
+   
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Tours and Travel Website",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400?grayscale",
+        description: "E-Commerce",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "TextUtils",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "News App",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Resume Builder",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Landing Page",
+        link: "#",
+    },
+
+  
+   
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "piyusgupta@gmail.com",
+    YOUR_FONE: "+91-8172879966",
+    description: "Crafting Delightful User Experiences: Let's Collaborate on UI/UX Design for Your Next Project ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
